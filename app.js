d7179ffa1cd5d96768d4449720cd97816a90f990
Pratik <raodeopratik@gmail.com>
2020-09-18 17:19:13 +0530

    To do List Assignment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 const app = express();
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
